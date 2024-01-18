9fcb812abf2b93041d4ef0c1b16ced1acb7c79ad
Ridwan Adebosin <adebosinadewale@gmail.com>
2024-01-18 22:51:07 +0100

    Components set up

--- src/App.js
+++ src/App.js
@@ -1,7 +1,39 @@
-import "./App.css";
-
-function App() {
-  return <div className="App"></div>;
+export default function App() {
+  return (
+    <div className="app">
+      <Logo />
+      <TextArea />
+      <PendingFlashCards />
+      <Stats />
+    </div>
+  );
 }
 
-export default App;
+function Logo() {
+  return <h2>🧬Medical Pending FlashCards🦠</h2>;
+}
+
+function TextArea() {
+  return (
+    <div className="add-form">
+      <h3>Write your pending work here</h3>
+      <textarea />
+    </div>
+  );
+}
+
+function PendingFlashCards() {
+  return <div className="list">FLASHCARDS</div>;
+}
+
+function PendingList() {}
+
+function Stats() {
+  return (
+    <footer className="stats">
+      <em>
+        You have X pendings on your handover log, and you've sorted X (X%)
+      </em>
+    </footer>
+  );
+}
